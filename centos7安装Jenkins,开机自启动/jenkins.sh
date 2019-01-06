1f7571cf193461b81a6861e6eda322650b5b1c28
jokfulStalker <jokfulStalker@qq.com>
2019-01-06 18:06:33 +0800

    Jenkins 安装和开机自启动配置

---
+++ centos7安装Jenkins,开机自启动/jenkins.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# chkconfig: 2345 20 90 
+# description: jenkins
+# This script will be executed *after* all the other init scripts.  
+# You can put your own initialization stuff in here if you don't  
+# want to do the full Sys V style init stuff.  
+#nohup $prefix/start_jenkins.sh >> $prefix/jenkins.log 2>&1 &
+
+
+JENKINS_ROOT=/opt/jenkins
+JENKINSFILENAME=jenkins.war
+
+#停止方法
+stop(){
+    echo "Stoping $JENKINSFILENAME "
+	ps -ef|grep $JENKINSFILENAME |awk '{print $2}'|while read pid
+	do
+	   kill -9 $pid
+	   echo " $pid kill"
+	done
+}
+
+case "$1" in
+start)
+    echo "Starting $JENKINSFILENAME "
+	nohup $JENKINS_ROOT/start_jenkins.sh >> $JENKINS_ROOT/jenkins.log 2>&1 &
+  ;;
+stop)
+  stop
+  ;;
+restart)
+  stop
+  start
+  ;;
+status)
+  ps -ef|grep $JENKINSFILENAME
+  ;;
+*)
+  printf 'Usage: %s {start|stop|restart|status}\n' "$prog"
+  exit 1
+  ;;
+esac
